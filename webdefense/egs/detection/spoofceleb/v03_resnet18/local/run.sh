ee672e750e9036d8e513dfd73efa969264201e37
FullBackward <1572758387@qq.com>
2026-03-21 07:11:28 +0000

    feat: removed webdefense

--- webdefense/egs/detection/spoofceleb/v03_resnet18/local/run.sh
+++
@@ -1,6 +0,0 @@
-
-spoofceleb_dir=/mnt/matylda4/qzhang/workspace/data/SpoofCeleb
-
-find $HOME/.cache/huggingface/hub/datasets--jungjee--spoofceleb -type l \
-    -exec ./move_to_here.sh ${spoofceleb_dir} {} +
-
--- webdefense/wedefense/deploy/s3prl_jit/__init__.py
+++
@@ -1,8 +0,0 @@
-# S3PRL JIT-compatible modules
-from .s3prl_frontend_jit import JITCompatibleS3prlFrontend
-from .jit_compatible_attention import JITCompatibleMultiheadAttention
-
-__all__ = [
-    'JITCompatibleS3prlFrontend',
-    'JITCompatibleMultiheadAttention',
-]
